d5fe25ea07d2f044008c888c077494df8705c041
Joshua Austen Trowell <54424388+JoshuaTrowell@users.noreply.github.com>
2019-11-18 12:18:45 -0500

    Update gis_data_finder.py

--- gis_data_finder.py
+++ gis_data_finder.py
@@ -1,13 +1,13 @@
 #set the map document we're working on
-mxd = arcpy.mapping.MapDocument(r"McCleary_CrimeDensity.mxd")
+mxd = arcpy.mapping.MapDocument(r"Trowell_GunViolence.mxd")
 #set the dataframe and layer we're searching in. 2nd parameter in df is the dataframe you want to work in, enter empty string
 #for default df; 2nd parameter in lyr is the layer you want to work in. Case sensitive!
-df = arcpy.mapping.ListDataFrames(mxd, "Saint Louis")[0]   
-lyr = arcpy.mapping.ListLayers(mxd, "countycrime", df)[0]
+df = arcpy.mapping.ListDataFrames(mxd, "DeathsbyFirearm")[0]   
+lyr = arcpy.mapping.ListLayers(mxd, "SoutheasternStates", df)[0]
 
 #create feature layer (temp layer) to copy to; 1st parameter input layer, 2nd parameter output layer
-arcpy.MakeFeatureLayer_management("McCleary_Crime.gdb/countycrime", "Rape_lyr")
+arcpy.MakeFeatureLayer_management("Trowell_GunViolence.gdb/SouthernStates", "SuppressedData_lyr")
 #select by attribute
-arcpy.SelectLayerByAttribute_management(lyr, "NEW_SELECTION", "UCR_CRIME_ = 111")
+arcpy.SelectLayerByAttribute_management(lyr, "NEW_SELECTION", "UCR_CRIME_ = Suppressed")
 #selection is made, time to copy to new layer from selection
-arcpy.CopyFeatures_management("Rape_lyr", "McCleary_Crime.gdb/rape_stats")
+arcpy.CopyFeatures_management("SuppressedData_lyr", "Trowell_GunViolence.gdb/Crude_Ra_1")
